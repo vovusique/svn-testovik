b815b06ef3372cf16e5be6b2d731f3f93c37ca70
vmanannikov <vmanannikov@broadsoft.com>
2018-03-29 15:27:25 +0300

    Some local changes 5

--- main.go
+++ main.go
@@ -18,4 +18,5 @@ func newfunc() {
 
 func newfunc2() {
 	fmt.Printf("I'm adding shiny new code and not touching legacy\n")
+	fmt.Printf("Doing separate stuff after bunch of changes and conflicts\n")
 }
