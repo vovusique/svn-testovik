c4143dc7ef6acd28e224261902b71292c1694a1a
vmanannikov <vmanannikov@broadsoft.com>
2018-03-29 15:18:47 +0300

    Some local changes 3

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	// just a small comment
 	fmt.Printf("Hello, world!!!!!!\n")
 	fmt.Printf("I'm starting to make change here\n")
+
+	// that is old code
 	fmt.Printf("Now there's a super-duper-muper updated line\n")
 }
